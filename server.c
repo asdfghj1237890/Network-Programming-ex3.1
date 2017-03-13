7c6b5462fee794529f3efecbc3983ee88aadd8ce
asdfghj1237890 <asdfghj1237890@yahoo.com.hk>
2017-03-13 06:35:08 -0700

    Add files via upload
    version 2

--- server.c
+++ server.c
@@ -5,32 +5,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #define MAXLINE 4096
 
 int main(int argc, char ** argv) 
 {
 	int listenfd, connfd;
-	socklen_t clilen;
+	socklen_t clilen = sizeof(struct sockaddr_in);
 	struct sockaddr_in servaddr, cliaddr;
 	char buf[MAXLINE + 1], *ip;
 	time_t ticks;
 	
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+        if( listenfd < 0 ) {
+          perror("Create socket failed.");
+          exit(1);
+        }
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(10127);
 	
-	servaddr.sin_port = htons(13);
-	
-	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	if( bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 ) {
+          perror("Bind socket failed.");
+          exit(1);
+        }
 	
 	listen(listenfd, 1024);
-	
-	for ( ; ; ) {
+	while(1){
 		puts("prepared to accept");
-		connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
-		ip = inet_ntoa(cliaddr.sin_addr);
-		printf("connection from %s, port %d\n", ip, cliaddr.sin_port);
+		connfd = accept(listenfd, (struct sockaddr*) &cliaddr,(socklen_t*)&clilen);
+		printf("connection from %s, port %d\n", inet_ntoa(cliaddr.sin_addr), cliaddr.sin_port);
 		ticks = time(NULL);
 		snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
 		write(connfd, buf, strlen(buf));
